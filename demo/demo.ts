75f8d7478d90826fcfa86e9185be7664c1c3a156
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-06 14:50:12 +0600

    Add demo

---
+++ demo/demo.ts
@@ -0,0 +1,12 @@
+import { Octokit } from "@octokit/rest";
+import {findAndReplaceInAllRepositories} from '../src/main.js'
+
+const octokit = new Octokit({ auth: 'YOUR_PERSONAL_ACCESS_TOKEN' });
+await findAndReplaceInAllRepositories({
+  octokit,
+  owner: 'deep-foundation',
+  path: '.github/workflows/npm-publish.yml',
+  regex: /uses: deep-foundation\/workflows\/.github\/workflows\/npm-publish.yml@npm-publish-.+/g,
+  replacement: 'uses: deep-foundation/workflows/.github/workflows/npm-publish.yml@npm-publish-4.0.1',
+  commitMessage: "Use npm-publish-4.0.1 version"
+})
