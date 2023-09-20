ea03c55979f8a9901fa74da65c60e971089c0ff0
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-20 17:14:58 +0600

    Update demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -9,8 +9,8 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 await findAndReplaceInAllRepositories({
   octokit,
   owner: 'deep-foundation',
-  path: '.github/workflows/npm-publish.yml',
-  regex: /uses: deep-foundation\/workflows\/.github\/workflows\/npm-publish.yml@npm-publish-.+/g,
-  replacement: 'uses: deep-foundation/workflows/.github/workflows/npm-publish.yml@npm-publish-4.0.1',
-  commitMessage: "Use npm-publish-4.0.1 version"
+  path: 'package.json',
+  regex: /myRegex/,
+  replacement: `myReplacement`,
+  commitMessage: `Replace myRegex with myReplacement`
 })
